b11aa4100a7f68ffa3d8556cf288efaf472e7b35
LeeJoonYong <dlwnsdyd17@naver.com>
2024-09-06 20:28:55 +0900

    5-6
    update lobby - Japan Rounge

--- TravelQuizMaster/Assets/scripts/teleportJO.cs
+++ TravelQuizMaster/Assets/scripts/teleportJO.cs
@@ -9,11 +9,24 @@ public class teleportJO : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+
+        if (other.CompareTag("Player"))                           // 플레이어 캐릭터가 만약
         {
-            if(gameObject.name == "telJO")
+            if(gameObject.name == "telJOlandmark")                // 일본 오사카 "랜드마크" 게임오브젝트에 부딪친다면
             {
-                SceneManager.LoadScene("JepenOsaka");
+                SceneManager.LoadScene("JepenOsaka_landmark");    // 일본 오사카 "랜드마크" 퀴즈 맵으로 이동한다.->
+            }
+            else if (gameObject.name == "telJOculture")           // 일본 오사카 "문화" 게임오브젝트에 부딪친다면
+            {
+                SceneManager.LoadScene("JepenOsaka_culture");     // 일본 오사카 "문화" 퀴즈 맵으로 이동한다.->
+            }
+            else if (gameObject.name == "telJOfood")              // 일본 오사카 "음식" 게임오브젝트에 부딪친다면
+            {
+                SceneManager.LoadScene("JepenOsaka_food");        // 일본 오사카 "음식" 퀴즈 맵으로 이동한다.->
+            }
+            else if (gameObject.name == "telJOtraffic")           // 일본 오사카 "교통" 게임오브젝트에 부딪친다면
+            {
+                SceneManager.LoadScene("JepenOsaka_traffic");     // 일본 오사카 "교통" 퀴즈 맵으로 이동한다.->
             }
             else if (gameObject.name == "telCS"){
                 SceneManager.LoadScene("ChinaShanghai");
